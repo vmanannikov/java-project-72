d0ecbed7f88be0fccbbb765c013a22cd1b6a828a
vmanannikov <manhetan@gmail.com>
2024-03-13 01:02:39 +0300

    add Dockerfile

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -18,7 +18,9 @@ public class App {
     public static Javalin getApp() {
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
-        });
+        })
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7070);
         return app;
     }
 }
